a6fce2d547d9b4651d8c3a996cecbd439c02ca3c
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-10-11 11:52:51 -0300

    feat: service and entity

--- src/main/java/com/demo/vitrine/model/Product.java
+++ src/main/java/com/demo/vitrine/model/Product.java
@@ -1,5 +1,81 @@
 package com.demo.vitrine.model;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 public class Product {
+
+    DecimalFormat decimalFormat = new DecimalFormat("R$ #,##0.00");
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String urlImage;
+
+    public Product(){
+    }
+
+    public Product(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public Product(Long id, String name, String description, BigDecimal price, String urlImage){
+        this.id = id;
+        this.name=name;
+        this.description=description;
+        this.price = price;
+        this.urlImage = urlImage;
+    }
     
+    public Long getId(){
+        return id;
+    }
+    
+    public void setId(Long id){
+        this.id=id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    @Override
+    public String toString(){
+        return "Informações Produto: " +
+            "Id: " + id.toString() +
+            "Name: " + name.toString() +
+            "Description: " + description.toString() +
+            "Price: " + decimalFormat.format(price) +
+            "Image URL: " + urlImage;            
+    }
 }
--- src/main/java/com/demo/vitrine/service/CatalogService.java
+++ src/main/java/com/demo/vitrine/service/CatalogService.java
@@ -1,5 +1,18 @@
 package com.demo.vitrine.service;
+import org.springframework.stereotype.Service;
 
+import com.demo.vitrine.model.Product;
+import java.util.List;
+import java.util.ArrayList;
+
+@Service
 public class CatalogService {
-    
+
+    private List<Product> products = new ArrayList<>();
+    private long proximoId = 1L;
+    public CatalogService(){}
+
+    public void registerProduct(Product product){
+
+    }
 }
